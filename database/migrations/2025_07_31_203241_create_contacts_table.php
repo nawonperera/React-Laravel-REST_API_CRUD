053a4782e4389d1da13dde05b96036bd586a7820
Nawon <nawon2794@gmail.com>
2025-08-01 02:12:57 +0530

    🎉 Initialize project & add Contacts model with Contacts migration table.

---
+++ database/migrations/2025_07_31_203241_create_contacts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable()->default(null);
+            $table->string('address')->nullable()->default(null);
+            $table->string('birth_date')->nullable()->default(null);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
